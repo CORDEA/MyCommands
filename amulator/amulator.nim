b287ec260d156e356a14c57ec9975db64c83c9a9
Yoshihiro Tanaka <contact@cordea.jp>
2015-12-07 09:18:34 +0900

    emulator search by api level

--- amulator/amulator.nim
+++ amulator/amulator.nim
@@ -35,12 +35,19 @@ proc getEmulators():seq[string] =
                         arr.add matches[0]
     return arr
 
+proc findApi(devices:openarray[string], expect:string):string =
+    var matches: array[1, string]
+    for device in devices:
+        if find(device, re"API_(\d{1,2})", matches) >= 0:
+            if matches[0] == expect:
+                return device
+
 proc launch(name: string, arch: string, is32bit: bool) =
     let envName = "ANDROID_HOME"
     if existsEnv(envName):
         let home = getEnv(envName)
         echo "found environment variable " & home
-        var prog = "/emulator"
+        var prog = "/tools/emulator"
         if is32bit:
             prog &= "-"
         else:
@@ -49,22 +56,35 @@ proc launch(name: string, arch: string, is32bit: bool) =
 
 when isMainModule:
     var
-        arch: string
+        arch: string = "x86"
         name: string
+        api: string
         is32bit: bool
 
     for kind, key, val in getopt():
         case kind
+        of cmdArgument:
+            name = val
         of cmdLongOption, cmdShortOption:
             case key
-            of "a", "arch":
-                arch = val
-            of "n", "name":
-                name = val
+            of "arch":
+                arch = if val == nil: arch else: val
+            of "a", "api":
+                api = val
             of "32bit":
                 is32bit = true
             else: discard
         else: discard
+    
+    let arr = getEmulators()
+
+    echo("name ", name, " api ", api)
+    if name == nil:
+        if api != nil:
+            name = findApi(arr, api)
+            echo("find api: ", (if name == nil: "not found" else: name))
+    if name == nil:
+        echo "name is nil, quit."
+        quit(1)
 
     launch(name, arch, is32bit)
-    discard getEmulators()
